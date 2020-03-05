3d298a5919e2676c80e2a1c37a1897fa610f993c
kunal jadhav <iamkunalhere@gmail.com>
2020-03-05 19:11:40 +0530

    Accepting Three values a,b and c from user

---
+++ SortingProblem.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+#Problem Statement: Sorting Arithmetic Computation Problem
+#Discription		: This solution computes diff arithmetic expr and sorts the results
+#Author		 		: Kunal Jadhav
+#Date					: 5 March 2020
+
+read -p "Enter the values of a,b and c: " a b c
