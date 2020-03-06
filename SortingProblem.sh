811de08dd213ef1c235d26975a296d8a4c7ea5f7
kunal jadhav <iamkunalhere@gmail.com>
2020-03-06 12:47:35 +0530

    Sorted array elements in Descending order

--- SortingProblem.sh
+++ SortingProblem.sh
@@ -16,14 +16,37 @@ function AcceptValues() {
 AcceptValues
 
 declare -A results
-results[operation1]=`expr "scale=2;$a+$b*$c" | bc -l`
-results[operation2]=`expr "scale=2;$a*$b+$c" | bc -l`
-results[operation3]=`expr "scale=2;$c+$a/$b" | bc -l`
-results[operation4]=`expr "scale=2;$a%$b+$c" | bc -l`
+results[operation1]=$(($a+$b*$c))
+results[operation2]=$(($a*$b+$c))
+results[operation3]=$(($c+$a/$b))
+results[operation4]=$(($a%$b+$c))
 
+declare -a resultsArray
 counter=0
 resultsArray[((counter++))]=${results[operation1]}
 resultsArray[((counter++))]=${results[operation2]}
 resultsArray[((counter++))]=${results[operation3]}
 resultsArray[((counter++))]=${results[operation4]}
+
+echo "Displaying array elements ${resultsArray[@]}"
+
+function DescendingSort(){
+for ((i=0; i<4; i++))
+do
+	temp=0
+	for ((j=0; j<4; j++))
+	do
+		if [[ ${resultsArray[$j]} -lt ${resultsArray[$i]} ]]
+		then
+			temp=${resultsArray[$i]}
+			resultsArray[$i]=${resultsArray[$j]}
+			resultsArray[$j]=$temp
+
+		fi
+	done
+done
+echo "Displaying in Descending order ${resultsArray[@]}"
+}
+
+DescendingSort
 
